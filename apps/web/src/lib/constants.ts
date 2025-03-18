3d8975d8825d2b326bbd021f7e740e1679374346
Yuri <isyuricunha@duck.com>
2025-03-18 09:13:11 -0300

    chore: some icon and links

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -2,10 +2,10 @@ export const isProduction = process.env.NODE_ENV === 'production'
 
 export const SITE_URL = isProduction ? 'https://yuricunha.com' : 'http://localhost:3000'
 
-export const GITHUB_USERNAME = 'tszhong0411'
+export const GITHUB_USERNAME = 'isyuricunha'
 
 export const SITE_NAME = 'Yuri Cunha'
-export const SITE_KEYWORDS = ['tszhong0411', 'Next.js', 'React', 'TypeScript', 'Node.js']
+export const SITE_KEYWORDS = ['isyuricunha', 'Next.js', 'React', 'TypeScript', 'Node.js']
 
 export const SITE_GITHUB_URL = 'https://github.com/isyuricunha'
 export const SITE_FACEBOOK_URL = 'https://www.facebook.com/isyuricunha'
